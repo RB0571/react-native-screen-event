182c838612b10c0219b3d2cfb9464d56be50b8ff
shuke <shukelin@126.com>
2017-11-21 16:14:07 +0800

    update.

--- ios/RCTScreenEvent.m
+++ ios/RCTScreenEvent.m
@@ -9,6 +9,8 @@
 #define NotificationChange CFSTR("com.apple.springboard.lockstate")
 #define NotificationPwdUI CFSTR("com.apple.springboard.hasBlankedScreen")
 
+static RCTScreenEvent* screenEvent = nil;
+
 @interface RCTScreenEvent()
 
 @end
@@ -22,15 +24,17 @@ RCT_EXPORT_MODULE(ScreenEvent);
 
 RCT_EXPORT_METHOD(create)
 {
+    screenEvent = self;
     //注册事件
-    CFNotificationCenterAddObserver(CFNotificationCenterGetDarwinNotifyCenter(), NULL, screenLockStateChanged, NotificationLock, (const void*)NULL, CFNotificationSuspensionBehaviorDeliverImmediately);
-    CFNotificationCenterAddObserver(CFNotificationCenterGetDarwinNotifyCenter(), NULL, screenLockStateChanged, NotificationChange, (const void*)NULL, CFNotificationSuspensionBehaviorDeliverImmediately);
+    CFNotificationCenterAddObserver(CFNotificationCenterGetDarwinNotifyCenter(), NULL, screenLockStateChanged, NotificationLock, NULL, CFNotificationSuspensionBehaviorDeliverImmediately);
+    CFNotificationCenterAddObserver(CFNotificationCenterGetDarwinNotifyCenter(), NULL, screenLockStateChanged, NotificationChange, NULL, CFNotificationSuspensionBehaviorDeliverImmediately);
 }
 
 RCT_EXPORT_METHOD(destroy)
 {
-    
-    
+    screenEvent = nil;
+    CFNotificationCenterRemoveObserver(CFNotificationCenterGetDarwinNotifyCenter(), NULL, NotificationLock, NULL);
+    CFNotificationCenterRemoveObserver(CFNotificationCenterGetDarwinNotifyCenter(), NULL, NotificationChange, NULL);
 }
 
 static void screenLockStateChanged(CFNotificationCenterRef center,void* observer,CFStringRef name,const void* object,CFDictionaryRef userInfo)
@@ -42,7 +46,9 @@ static void screenLockStateChanged(CFNotificationCenterRef center,void* observer
     } else {
         NSLog(@"======================lock state changed.");
         // 此处监听到屏幕解锁事件（锁屏也会掉用此处一次，锁屏事件要在上面实现）
-        [appDelegate.eventEmitter sendEventWithName:@"com.apple.springboard.lockstate" body:nil];
+        if (screenEvent) {
+            [screenEvent.bridge.eventDispatcher sendAppEventWithName:@"com.apple.springboard.lockstate"  body:nil];
+        }
     }
 }
 @end
